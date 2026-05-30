eeb318de8290fa848527e4a597a2ac0e821bfd50
wannabebritgames <andrewstogner264@gmail.com>
2026-05-29 22:09:07 -0500

    Update entities.js

--- entities.js
+++ entities.js
@@ -1,24 +1,22 @@
 class Snake {
-    constructor(x,y,name,color,isPlayer=false){
-        this.x=x;
-        this.y=y;
-        this.angle=0;
-        this.speed=2;
-        this.length=10;
-        this.body=[];
-        this.name=name;
-        this.color=color;
-        this.isPlayer=isPlayer;
-        this.mass=10;
+    constructor(x,y,color,isPlayer=false){
+        this.x = x;
+        this.y = y;
+        this.angle = 0;
+        this.speed = 2.5;
+        this.body = [];
+        this.length = 20;
+        this.color = color;
+        this.isPlayer = isPlayer;
     }
 
     move(){
-        this.x += Math.cos(this.angle)*this.speed;
-        this.y += Math.sin(this.angle)*this.speed;
+        this.x += Math.cos(this.angle) * this.speed;
+        this.y += Math.sin(this.angle) * this.speed;
 
         this.body.push({x:this.x,y:this.y});
 
-        while(this.body.length > this.length){
+        if(this.body.length > this.length){
             this.body.shift();
         }
     }
@@ -26,15 +24,7 @@ class Snake {
 
 class Food {
     constructor(x,y){
-        this.x=x;
-        this.y=y;
-    }
-}
-
-class PowerUp {
-    constructor(x,y,type){
-        this.x=x;
-        this.y=y;
-        this.type=type;
+        this.x = x;
+        this.y = y;
     }
 }
